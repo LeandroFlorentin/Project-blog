89a48c785df375f63cbfa14a5ca47cd9efc52966
unknown <leandro.florentin@hotmail.com>
2024-03-05 00:24:45 -0300

    prisma-sin-funcionar

--- ms/ms-seguridad/src/prisma/prisma.service.ts
+++ ms/ms-seguridad/src/prisma/prisma.service.ts
@@ -1,9 +1,11 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
-}
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
+export default prisma;
--- ms/ms-seguridad/src/user/user.service.ts
+++ ms/ms-seguridad/src/user/user.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UserLogin } from './user.interfaces';
-import { PrismaService } from 'src/prisma/prisma.service';
+import prisma from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {
-
-  }
   loginUsers(user: UserLogin) {
     return {
       info: true,
       msg: "Info OK",
       content: {
-        token: this.prisma.users.findMany()
+        token: prisma.users.findMany()
       }
     }
   }
